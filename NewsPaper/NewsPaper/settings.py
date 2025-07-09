acff36bd33cb78d7e7034d7c0aafb4fe9fb97614
Ushakov29 <Ushakov29@yandex.ru>
2025-07-09 13:37:10 +0300

    Изменения 9.07.2025

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -5,15 +5,10 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^b48k3u1s728@-1*d8g26_!7*c%ucz$pf9v4r@5s2jx$$53hny'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# В зависимости от окружения, укажите допустимые хосты.
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
@@ -26,8 +21,24 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'news',
     'accounts',
-    'django_filters',  # django_filters должен быть установлен отдельно
+    'django_filters',           # Для фильтрации
+    'django_extensions',        # Дополнительные инструменты для разработки
+    'allauth',                  # Для управления аккаунтами
+    'allauth.account',          # Поддержка аккаунта
+    'allauth.socialaccount',    # Социальные аккаунты
+    'django_apscheduler',       # Планировщик задач
 ]
+
+# Настройки для django-allauth
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',  # Стандартный бэкенд
+    'allauth.account.auth_backends.AuthenticationBackend',  # Бэкенд django-allauth
+)
+
+SITE_ID = 1  # Не забудь указать ID сайта для allauth
+
+LOGIN_REDIRECT_URL = '/'  # URL после успешного входа
+LOGOUT_REDIRECT_URL = '/'  # URL после выхода из системы
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -35,6 +46,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -45,8 +57,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),  # Шаблоны проекта
-            os.path.join(BASE_DIR, 'news', 'templates', 'news'),  # Шаблоны новостей
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'news', 'templates', 'news'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -66,7 +78,7 @@ WSGI_APPLICATION = 'NewsPaper.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # База данных
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -95,12 +107,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 
-# На реальном сервере используйте STATIC_ROOT для сбора статических файлов
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Папка со статическими файлами
+    BASE_DIR / "static",
 ]
+
+# Опционально: настройки для Celery (если используешь)
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'  # URL брокера
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
